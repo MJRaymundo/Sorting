8a04a37ebd11176057e05d9fa1f37b03e23dec88
[MJRaymundo] <[m4rkjohnraymundo+github@gmail.com]>
2023-01-26 21:31:24 +0800

    First commit

---
+++ Selection Sort/Selection Sort.py
@@ -0,0 +1,4 @@
+# Mark john M. Raymundo CoE 2-2 Data Structure and Algorithms
+#Sorting: Selection Sort
+#Given Array Values: 67, 77, 47, 15, 1, 25, 59, 46, 8, 87
+#Source: https://www.youtube.com/watch?v=ee80YmiaSVQ&list=PLliXbzY3XhUSJy3izXH-0ojiT3Uup8xbu&index=2
