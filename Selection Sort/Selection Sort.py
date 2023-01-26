f6c9aec319318e1988d43d711182f053a9f3c721
[MJRaymundo] <[m4rkjohnraymundo+github@gmail.com]>
2023-01-26 21:40:02 +0800

    Defined Selection Sort Function

--- Selection Sort/Selection Sort.py
+++ Selection Sort/Selection Sort.py
@@ -2,3 +2,13 @@
 #Sorting: Selection Sort
 #Given Array Values: 67, 77, 47, 15, 1, 25, 59, 46, 8, 87
 #Source: https://www.youtube.com/watch?v=ee80YmiaSVQ&list=PLliXbzY3XhUSJy3izXH-0ojiT3Uup8xbu&index=2
+
+def selection_sort(ArrayValue):
+    for i in range(0, len(ArrayValue) - 1):
+        cur_min_idx = i
+        for j in range (i + 1, len(ArrayValue)):
+            if ArrayValue[j] < ArrayValue[cur_min_idx]:
+                cur_min_idx = j
+
+        #Swapping Lowest Value to Current Position
+        ArrayValue[i], ArrayValue[cur_min_idx] = ArrayValue[cur_min_idx], ArrayValue[i]
