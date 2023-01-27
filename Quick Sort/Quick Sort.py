5b12292a6d4255b55bf1fa7f714dd858259baee6
[MJRaymundo] <[m4rkjohnraymundo+github@gmail.com]>
2023-01-27 16:15:17 +0800

    Defined Qucksort Function and checking if the length of ArrayValue is greater than 1

--- Quick Sort/Quick Sort.py
+++ Quick Sort/Quick Sort.py
@@ -3,6 +3,11 @@
 #Given Array Values: 67, 77, 47, 15, 1, 25, 59, 46, 8, 87
 #Source: https://www.youtube.com/watch?v=9KBwdDEwal8&list=PLliXbzY3XhUSJy3izXH-0ojiT3Uup8xbu&index=4
 
+def QuickSort(ArrayValue, Left, Right):
+    if Left < Right:
+        PartitionPosition = Partition(ArrayValue, Left, Right)
+
+
 ArrayValue = [67, 77, 47, 15, 1, 25, 59, 46, 8, 87]
 QuickSort(ArrayValue)
 
