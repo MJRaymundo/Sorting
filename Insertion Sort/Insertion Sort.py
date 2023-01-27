f77fd8ef0a8d0edc6741fb29d6af802720a52a2b
[MJRaymundo] <[m4rkjohnraymundo+github@gmail.com]>
2023-01-27 15:08:37 +0800

    Added InsertionSort Definition

--- Insertion Sort/Insertion Sort.py
+++ Insertion Sort/Insertion Sort.py
@@ -3,6 +3,15 @@
 #Given Array Values: 67, 77, 47, 15, 1, 25, 59, 46, 8, 87
 #Source: https://www.youtube.com/watch?v=R_wDA-PmGE4&list=PLliXbzY3XhUSJy3izXH-0ojiT3Uup8xbu&index=1
 
+def InsertionSort(ArrayValue):
+    for i in range(1, len(ArrayValue)):
+        j = i
+        while ArrayValue[j-1] > ArrayValue[j] and j > 0:
+            #Swapping Values
+            ArrayValue[j-1], ArrayValue[j] = ArrayValue[j], ArrayValue[j-1]
+            j -= 1
+            
+
 ArrayValue = [67, 77, 47, 15, 1, 25, 59, 46, 8, 87]
 InsertionSort(ArrayValue)
 
