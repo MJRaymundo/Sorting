4f6dc25f3b602816c3d8e29e3a1e8f3fc03c18cc
[MJRaymundo] <[m4rkjohnraymundo+github@gmail.com]>
2023-01-27 16:23:22 +0800

    Added Partition Function Definition

--- Quick Sort/Quick Sort.py
+++ Quick Sort/Quick Sort.py
@@ -8,8 +8,24 @@ def QuickSort(ArrayValue, Left, Right):
         PartitionPosition = Partition(ArrayValue, Left, Right)
         QuickSort(ArrayValue, Left, PartitionPosition - 1)
         QuickSort(ArrayValue, Left, PartitionPosition + 1)
-        
+
 def Partition(ArrayValue, Left, Right):
+    i = Left
+    j = Right - 1
+    Pivot = ArrayValue[Right]
+
+    while i < j:
+        while i < Right and ArrayValue[i] < Pivot:
+            i += 1
+        while j > Left and ArrayValue[j] >= Pivot:
+            j -= 1
+        #Swapping Positions
+        if i < j:
+            ArrayValue[i], ArrayValue[j] = ArrayValue[j], ArrayValue[i]
+    #Swapping Positions
+    if ArrayValue[i] > Pivot:
+        ArrayValue[i], ArrayValue[Right] = ArrayValue[Right], ArrayValue[i]
+    return i
 
 
 ArrayValue = [67, 77, 47, 15, 1, 25, 59, 46, 8, 87]
