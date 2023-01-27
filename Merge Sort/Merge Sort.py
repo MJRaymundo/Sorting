299feb5edd9ded8cf5569a7abddf24edb6cb1ecd
[MJRaymundo] <[m4rkjohnraymundo+github@gmail.com]>
2023-01-27 15:36:23 +0800

    Added MergeSort Function

--- Merge Sort/Merge Sort.py
+++ Merge Sort/Merge Sort.py
@@ -3,6 +3,37 @@
 #Given Array Values: 67, 77, 47, 15, 1, 25, 59, 46, 8, 87
 #Source: https://www.youtube.com/watch?v=cVZMah9kEjI&list=PLliXbzY3XhUSJy3izXH-0ojiT3Uup8xbu&index=3
 
+def MergeSort(ArrayValue):
+    if len(ArrayValue) > 1:
+        LeftArrayValue = ArrayValue[:len(ArrayValue)//2]
+        RightArrayValue = ArrayValue[len(ArrayValue)//2:]
+
+        #Recursion
+        MergeSort(LeftArrayValue)
+        MergeSort(RightArrayValue)
+
+        #Merge
+        i = 0 #LeftArrayValue idx
+        j = 0 #RightArrayValue idx
+        k = 0 #Merged ArrayValue idx
+        while i < len(LeftArrayValue) and j < len(RightArrayValue):
+            if LeftArrayValue[i] < RightArrayValue[j]:
+                ArrayValue[k] = LeftArrayValue[i]
+                i+=1
+            else:
+                ArrayValue[k] = RightArrayValue[j]
+                j +=1
+            k +=1
+        while i < len(LeftArrayValue):
+            ArrayValue[k] = LeftArrayValue[i]
+            i += 1
+            k += 1
+
+        while j < len(RightArrayValue):
+            ArrayValue[k] = RightArrayValue[j]
+            j+=1
+            k+=1
+
 ArrayValue = [67, 77, 47, 15, 1, 25, 59, 46, 8, 87]
 MergeSort(ArrayValue)
 
