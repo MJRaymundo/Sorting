a169b02907dd329d11d2118775f5f5c401f53a88
[MJRaymundo] <[m4rkjohnraymundo+github@gmail.com]>
2023-01-27 15:16:55 +0800

    Added ArrayValue

--- Merge Sort/Merge Sort.py
+++ Merge Sort/Merge Sort.py
@@ -2,3 +2,8 @@
 #Sorting: Merge Sort
 #Given Array Values: 67, 77, 47, 15, 1, 25, 59, 46, 8, 87
 #Source: https://www.youtube.com/watch?v=cVZMah9kEjI&list=PLliXbzY3XhUSJy3izXH-0ojiT3Uup8xbu&index=3
+
+ArrayValue = [67, 77, 47, 15, 1, 25, 59, 46, 8, 87]
+MergeSort(ArrayValue)
+
+print("Final Array Value",ArrayValue)
