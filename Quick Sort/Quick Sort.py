e8073de6322dd80d7fd8113ab5f03de120fa2369
[MJRaymundo] <[m4rkjohnraymundo+github@gmail.com]>
2023-01-27 16:10:36 +0800

    Added ArrayValue

--- Quick Sort/Quick Sort.py
+++ Quick Sort/Quick Sort.py
@@ -2,3 +2,8 @@
 #Sorting: Quick Sort
 #Given Array Values: 67, 77, 47, 15, 1, 25, 59, 46, 8, 87
 #Source: https://www.youtube.com/watch?v=9KBwdDEwal8&list=PLliXbzY3XhUSJy3izXH-0ojiT3Uup8xbu&index=4
+
+ArrayValue = [67, 77, 47, 15, 1, 25, 59, 46, 8, 87]
+QuickSort(ArrayValue)
+
+print("Final Array Value",ArrayValue)
