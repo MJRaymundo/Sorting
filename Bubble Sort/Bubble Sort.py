5d6ffd7fd2676c283b2ecf0201f070bfeaa51e72
[MJRaymundo] <[m4rkjohnraymundo+github@gmail.com]>
2023-01-27 14:46:46 +0800

    Added Printing Function every change in Position and Array Value

--- Bubble Sort/Bubble Sort.py
+++ Bubble Sort/Bubble Sort.py
@@ -10,6 +10,8 @@ def BubbleSort(ArrayValue):
                 Temp = ArrayValue[j]
                 ArrayValue[j] = ArrayValue[j+1]
                 ArrayValue[j+1] = Temp
+        print("Current Position: ", i)
+        print("Array Value", ArrayValue)
 
 ArrayValue = [67, 77, 47, 15, 1, 25, 59, 46, 8, 87]
 BubbleSort(ArrayValue)
