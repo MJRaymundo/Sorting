e2f71f1086f5aadb16ad4abfdeebe7b3f87ff18f
[MJRaymundo] <[m4rkjohnraymundo+github@gmail.com]>
2023-01-27 14:43:26 +0800

    Added BubbleSort Function Definition

--- Bubble Sort/Bubble Sort.py
+++ Bubble Sort/Bubble Sort.py
@@ -3,6 +3,14 @@
 #Given Array Values: 67, 77, 47, 15, 1, 25, 59, 46, 8, 87
 #Source: https://www.youtube.com/watch?v=Vca808JTbI8
 
+def BubbleSort(ArrayValue):
+    for i in range (len(ArrayValue)-1, 0, -1):
+        for j in range (i):
+            if ArrayValue[j]>ArrayValue[j+1]:
+                Temp = ArrayValue[j]
+                ArrayValue[j] = ArrayValue[j+1]
+                ArrayValue[j+1] = Temp
+
 ArrayValue = [67, 77, 47, 15, 1, 25, 59, 46, 8, 87]
 BubbleSort(ArrayValue)
 
